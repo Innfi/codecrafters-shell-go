1f679a0a7b5db26911ee50f546309e0b3aedd4c1
innfi <innfi.world@gmail.com>
2025-01-05 20:58:32 +0900

    loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,16 +8,18 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		// Wait for user input
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		os.Exit(1)
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			os.Exit(1)
+		}
+
+		command = strings.Replace(command, "\r", "", -1)
+		command = strings.Replace(command, "\n", "", -1)
+
+		fmt.Printf("%s: command not found\n", command)
 	}
-
-	command = strings.Replace(command, "\r", "", -1)
-	command = strings.Replace(command, "\n", "", -1)
-
-	fmt.Printf("%s: command not found", command)
 }
