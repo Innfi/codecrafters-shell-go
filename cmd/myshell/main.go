eaf6d78488c9417deb79d234392b67204369de95
innfi <innfi.world@gmail.com>
2025-01-06 20:12:48 +0900

    echo

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -12,18 +12,21 @@ func main() {
 		// Wait for user input
 		fmt.Fprint(os.Stdout, "$ ")
 
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		arguments, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			os.Exit(1)
 		}
+		arguments = strings.Replace(arguments, "\r", "", -1)
+		arguments = strings.Replace(arguments, "\n", "", -1)
 
-		command = strings.Replace(command, "\r", "", -1)
-		command = strings.Replace(command, "\n", "", -1)
-
-		if command[0:4] == "exit" {
+		command, params, _ := strings.Cut(arguments, " ")
+		if command == "echo" {
+			fmt.Println(params)
+			continue
+		} else if arguments[0:4] == "exit" {
 			os.Exit(0)
 		}
 
-		fmt.Printf("%s: command not found\n", command)
+		fmt.Printf("%s: command not found\n", arguments)
 	}
 }
