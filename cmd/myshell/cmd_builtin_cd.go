4fa146789758396284be7a76ebed83c717061152
innfi <innfi.world@gmail.com>
2025-01-11 20:19:04 +0900

    typos

--- cmd/myshell/cmd_builtin_cd.go
+++ cmd/myshell/cmd_builtin_cd.go
@@ -8,6 +8,6 @@ import (
 func HandleCommandCd(argument *Argument) {
 	if err := os.Chdir(argument.params); err != nil {
 		// fmt.Println(err)
-		fmt.Printf("cd: %s: no such file or directory\n", argument.params)
+		fmt.Printf("cd: %s: No such file or directory\n", argument.params)
 	}
 }
