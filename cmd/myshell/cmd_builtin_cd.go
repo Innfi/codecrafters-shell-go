19bdda61adaf5a2401205c358ebd30d841e2b491
innfi <innfi.world@gmail.com>
2025-01-12 21:49:11 +0900

    home

--- cmd/myshell/cmd_builtin_cd.go
+++ cmd/myshell/cmd_builtin_cd.go
@@ -6,8 +6,22 @@ import (
 )
 
 func HandleCommandCd(argument *Argument) {
-	if err := os.Chdir(argument.params); err != nil {
-		// fmt.Println(err)
-		fmt.Printf("cd: %s: No such file or directory\n", argument.params)
+	effectivePath := toEffectivePath(argument)
+	fmt.Printf("effectivePath: %s\n", effectivePath)
+	if err := os.Chdir(effectivePath); err != nil {
+		fmt.Printf("cd: %s: No such file or directory\n", effectivePath)
 	}
+}
+
+func toEffectivePath(argument *Argument) string {
+	if argument.params[0] != '~' {
+		return argument.params
+	}
+
+	home := os.Getenv("HOME")
+	if len(argument.params) > 1 {
+		return fmt.Sprintf("%s%s", home, argument.params[1:])
+	}
+
+	return home
 }
